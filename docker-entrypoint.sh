4434ccb0c20d06df505f9274f066d83a330c3bee
Anthony Re <anthonyjosephre@gmail.com>
2018-12-22 15:56:43 -0500

    fixed bug in which docker entrypoint failed on container restart due to existing user

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,8 +2,8 @@
 
 set -euo pipefail
 
-groupadd --g ${GID} rust
-useradd -u ${UID} -g ${GID} -ms /bin/bash rust
+id -g rust &> /dev/null || groupadd --g ${GID} rust
+id -u rust &> /dev/null || useradd -u ${UID} -g ${GID} -ms /bin/bash rust
 
 chown -R ${UID}:${GID} /workdir
 chown -R ${UID}:${GID} /usr/local/cargo
